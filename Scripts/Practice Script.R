c643315c3a27b3ed14a2c67812a11998ddcb0a43
Jane <jane@example.com>
2021-07-16 14:25:21 -0400

    scatter

--- Scripts/Practice Script.R
+++ Scripts/Practice Script.R
@@ -1,1 +1,17 @@
 ###Practice Script
+library(tidyverse)
+library(ggplot2)
+
+data <- read.csv("Data/benthic_invertebrates_univariate.csv")
+colnames(data)
+
+plot <- ggplot(data, aes(x = Depth, y = abundance)) +
+  geom_point()
+plot
+
+ggsave("Figures/scatter_abundance.jpeg",
+       plot,
+       height = 7.21,
+         width= 7.52,
+         units = "in",
+       dpi = 500)
